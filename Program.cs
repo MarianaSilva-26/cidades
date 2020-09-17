b44e05609dca60f535cdd1e8272b7718b764ecc6
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-09-17 13:40:27 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("guaruja!");
+            Console.WriteLine("santos!");
+            Console.WriteLine("cubatão!");
+            Console.WriteLine("são vicente!");
+            Console.WriteLine("praia grande!");
+            Console.WriteLine("mongaguá!");
+        }
+    }
+}
